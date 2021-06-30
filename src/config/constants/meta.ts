9d9383e8eb83ccb7045935bbf290f9047ece116b
rpatterson9 <40555490+rpatterson9@users.noreply.github.com>
2021-06-30 16:33:18 -0700

    more

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -5,7 +5,7 @@ export const DEFAULT_META: PageMeta = {
   title: 'MegaYields',
   description:
     'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+  image: 'https://megayields.netlify.app/images/hero.png',
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
@@ -28,15 +28,15 @@ export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
       }
     case '/pools':
       return {
-        title: `${t('Pools')} | ${t('PancakeSwap')}`,
+        title: `${t('Pools')} | ${t('MegaYields')}`,
       }
     case '/lottery':
       return {
-        title: `${t('Lottery')} | ${t('PancakeSwap')}`,
+        title: `${t('Lottery')} | ${t('MegaYields')}`,
       }
     case '/collectibles':
       return {
-        title: `${t('Collectibles')} | ${t('PancakeSwap')}`,
+        title: `${t('Collectibles')} | ${t('MegaYields')}`,
       }
     case '/ifo':
       return {
@@ -52,7 +52,7 @@ export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
       }
     case '/profile':
       return {
-        title: `${t('Your Profile')} | ${t('PancakeSwap')}`,
+        title: `${t('Your Profile')} | ${t('MegaYields')}`,
       }
     default:
       return null
