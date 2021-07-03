55a2b01224505166b4d2340167afc668664f2748
robert patterson <rob@robpatterson.net>
2021-07-02 19:08:48 -0700

    more

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -11,51 +11,23 @@ const GlobalStyle = createGlobalStyle`
   * {
     font-family: 'Kanit', sans-serif;
   }
-  body {
+  body  {   
     background-color: ${({ theme }) => theme.colors.background};
     .dwqKIT .desktop-icon {
       width: 0px;
       display: none;
-    }
-      .desktop-icon {
-        width: 0px;
-        display: none;
-  }
-  .sc-jSFjdj sc-ljsmAU kJmatq gpIDbW {
-    background: #000000;
-  }
-  .sc-jSFjdj jxszLn sc-jlZJtj QgNfY {
-    background: #000000;
-  }
-  .sc-jSFjdj jxszLn sc-jlZJtj QgNfY {
-    background: #000000;
+    },
 
-  }
-}
-
-  .jcNvwq {
-    width: 0px;
-    display: none;
-  }
-  .sc-jSFjdj sc-ljsmAU kJmatq gpIDbW {
-    background: #000000;
-  }
-  .sc-jSFjdj jxszLn sc-jlZJtj QgNfY {
-    background: #000000;
-  }
-  .sc-jSFjdj jxszLn sc-jlZJtj QgNfY {
-    background: #000000;
-
-  }
-  .QgNfY {
-    background: #000000;
-  }
+    .sc-jSFjdj jxszLn sc-jlZJtj QgNfY {
+      background: #000000; }
+  
 
     img {
       height: auto;
       max-width: 100%;
     }
   }
-`
+}
+
 
 export default GlobalStyle
